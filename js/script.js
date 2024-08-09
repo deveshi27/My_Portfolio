0a09af7eed97043fbd08fde1ee73a8441fa65065
Deveshi Nema <127538017+deveshi27@users.noreply.github.com>
2024-08-09 02:49:19 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
 // typed js
 
 const typed = new Typed('.multiple-text',{
-      strings: [' Tech Learner' , ' Java Learner' , ' Front-end Beginner'],
+      strings: [' Tech Learner' , 'Aspiring Python/Django Developer' , ' Front-end Beginner'],
       typeSpeed:100,
       backSpeed:100,
       backDelay:1000,
